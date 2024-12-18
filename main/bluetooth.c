b3f154814fc6549285a93a1e2af041ad3742215b
Daniel Yadov <danielyadov103@gmail.com>
2024-12-17 19:45:32 -0600

    resetting computer

--- main/bluetooth.c
+++ main/bluetooth.c
@@ -25,8 +25,7 @@ static uint8_t char_uuid[16] = {
 static uint16_t gatts_handle_table[1];
 
 // GATT Event Handler
-static void gatts_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_if,
-                                esp_ble_gatts_cb_param_t *param) {
+static void gatts_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_if, esp_ble_gatts_cb_param_t *param) {
     switch (event) {
         case ESP_GATTS_WRITE_EVT:
             if (param->write.handle == gatts_handle_table[0]) {
@@ -63,7 +62,7 @@ void ble_server_init(void) {
     esp_ble_gatts_app_register(ESP_APP_ID);
 
     // Create BLE service and add characteristic
-    esp_ble_gatts_create_attr_tab((esp_gatts_attr_db_t[]){
+    esp_ble_gatts_create_attr_tab(esp_gatts_attr_db_t[]){
         {ESP_GATT_AUTO_RSP, ESP_UUID_LEN_16, (uint8_t *)service_uuid, ESP_GATT_PERM_READ | ESP_GATT_PERM_WRITE},
         {ESP_GATT_AUTO_RSP, ESP_UUID_LEN_16, (uint8_t *)char_uuid, ESP_GATT_PERM_WRITE, NULL, 100}  // Writable characteristic
     }, ESP_GATT_IF_NONE, 2, SVC_INST_ID);
